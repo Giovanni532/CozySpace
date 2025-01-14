607f48fbf43f30a44c4aadd85dcee3869fc78566
Salcuni Giovanni <salcuni@PALEXPO-LT-GS.orgex.po>
2025-01-14 09:33:29 +0100

    Ajout de prisma

--- app/auth/action/authAction.ts
+++ app/auth/action/authAction.ts
@@ -1,18 +1,26 @@
 "use server";
 
 import supabase from "@/lib/supabase";
+import { z } from "zod";
 
-export async function signup(email: string, password: string) {
-    const { data, error } = await supabase.auth.signUp({
-        email,
-        password,
-    });
+const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(4),
+    firstName: z.string().min(1),
+    lastName: z.string().min(1),
+});
 
-    if (error) {
-        throw new Error(error.message);
-    }
+export async function signup(email: string, password: string, firstName: string, lastName: string) {
+    // const { data, error } = await supabase.auth.signUp({
+    //     email,
+    //     password,
+    // });
 
-    return data;
+    // if (error) {
+    //     throw new Error(error.message);
+    // }
+
+    // return data;
 }
 
 export async function login(email: string, password: string) {
